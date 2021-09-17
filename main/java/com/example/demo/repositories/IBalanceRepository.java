ccd7d6bbae25e6281f0bfa1bba6f3d2f54d89e30
Funkiefash <42506831+Funkiefash@users.noreply.github.com>
2021-09-17 16:28:09 +0100

    Update IBalanceRepository.java

--- main/java/com/example/demo/repositories/IBalanceRepository.java
+++ main/java/com/example/demo/repositories/IBalanceRepository.java
@@ -2,11 +2,11 @@ package com.example.demo.repositories;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface IBalanceRepository extends CrudRepository<Balances, Long>
+public interface IBalanceRepository extends JpaRepository<Balances, Long>
 {
 
 }
