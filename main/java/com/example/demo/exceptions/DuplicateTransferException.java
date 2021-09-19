3b38a3a843698cc77e0acb460ded6232af93e445
Funkiefash <42506831+Funkiefash@users.noreply.github.com>
2021-09-20 00:33:29 +0100

    Delete main directory

--- main/java/com/example/demo/exceptions/DuplicateTransferException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exceptions;
-
-class DuplicateTransferException extends RuntimeException {
-
-   DuplicateTransferException(Long TransactionReference) {
-        super("Duplicate Transfer,Please try again " + TransactionReference);
-    }
-}
