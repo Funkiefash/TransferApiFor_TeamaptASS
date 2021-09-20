e7d255fc5d81c3cf03a2949ecdb2b0453662f695
Funkiefash <42506831+Funkiefash@users.noreply.github.com>
2021-09-20 11:01:04 +0100

    Update and rename IBalanceRepository.java to BalancesRepository.java

--- main/java/com/example/demo/repositories/BalancesRepository.java
+++ main/java/com/example/demo/repositories/BalancesRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface IBalanceRepository extends JpaRepository <Balances, Long>
+public interface BalancesRepository extends JpaRepository <Balances, Long>
 {
 
 }
