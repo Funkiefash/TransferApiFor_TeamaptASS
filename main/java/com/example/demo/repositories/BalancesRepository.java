b76db4095a1b206b5ce3a092b003fa038615dd49
Funkiefash <42506831+Funkiefash@users.noreply.github.com>
2021-09-22 21:31:45 +0100

    Update BalancesRepository.java

--- main/java/com/example/demo/repositories/BalancesRepository.java
+++ main/java/com/example/demo/repositories/BalancesRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repositories;
 
 import java.util.List;
+import java.util.Optional;
 import com.example.demo.repositories.BalancesRepository;
 import com.example.demo.Service.BalanceService;
 import com.example.demo.Entity.Balances;
@@ -13,6 +14,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface BalancesRepository extends JpaRepository <Balances, Long>
 {
-List<Balances>findByAccountnr(String accountnr);
-    List<Balances> getBalancesForUpdate(String accountnr);
+Optional<Balances>findByAccountnr(String accountnr);
+    Optional<Balances> getBalancesForUpdate(String accountnr);
 }
